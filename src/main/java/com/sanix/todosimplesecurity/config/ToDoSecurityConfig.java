d3294006de5a11deb3908528536ab3d027f65649
sanix <sanicksikani@gmail.com>
2020-09-21 00:59:25 +0300

    Overriding default login page

--- src/main/java/com/sanix/todosimplesecurity/config/ToDoSecurityConfig.java
+++ src/main/java/com/sanix/todosimplesecurity/config/ToDoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.sanix.todosimplesecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -21,5 +22,13 @@ public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .httpBasic();
     }
 }
