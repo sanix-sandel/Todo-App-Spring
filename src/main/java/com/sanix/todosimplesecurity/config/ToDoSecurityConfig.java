abef10daa30720c2965f8570f74664b199b2d7b6
sanix <sanicksikani@gmail.com>
2020-09-21 01:34:42 +0300

    Custom login page

--- src/main/java/com/sanix/todosimplesecurity/config/ToDoSecurityConfig.java
+++ src/main/java/com/sanix/todosimplesecurity/config/ToDoSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.sanix.todosimplesecurity.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -27,8 +29,18 @@ public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
+                .requestMatchers(
+                        PathRequest
+                            .toStaticResources()
+                        .atCommonLocations()).permitAll()
                 .anyRequest().fullyAuthenticated()
                 .and()
-                .httpBasic();
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                    .logoutRequestMatcher(
+                            new AntPathRequestMatcher("/logout")
+                    )
+                .logoutSuccessUrl("/login");
     }
 }
