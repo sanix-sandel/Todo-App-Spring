dbc62b09b1460b7bc7353c0adfbb06840295bb6f
sanix <sanicksikani@gmail.com>
2020-09-21 01:39:01 +0300

    final

--- src/main/java/com/sanix/todosimplesecurity/config/ToDoSecurityConfig.java
+++ src/main/java/com/sanix/todosimplesecurity/config/ToDoSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import static org.hibernate.criterion.Restrictions.and;
+
 @Configuration
 public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -41,6 +43,8 @@ public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutRequestMatcher(
                             new AntPathRequestMatcher("/logout")
                     )
-                .logoutSuccessUrl("/login");
+                    .logoutSuccessUrl("/login")
+                .and()
+                    .httpBasic();
     }
 }
